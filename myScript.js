a6c3c0a02e843159ffb4538fd52a64dc15d88985
zzy1127 <1726073424@qq.com>
2022-08-31 18:12:23 +0800

    添加了JS点击搜索框后改变背景亮度的效果

---
+++ myScript.js
@@ -0,0 +1,13 @@
+function changeBrightness(){
+    document.body.style.backgroundImage="url("+"images/img-dark.png"+")";
+    document.getElementById("logo").src="images/logo-dark.png";
+    document.getElementById("bar").src="images/bar-dark.png";
+    document.getElementById("login").src="images/login-dark.png";
+}
+
+function rechange(){
+    document.body.style.backgroundImage="url("+"images/img.png"+")";
+    document.getElementById("logo").src="images/logo.png";
+    document.getElementById("bar").src="images/bar.png";
+    document.getElementById("login").src="images/login.png";
+}
